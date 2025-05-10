1a4713894ce49ee5d7d137b6cc56b198ea6b31ed
James Watt <james.watt529@outlook.com>
2025-05-10 18:39:03 +0100

    Configure SEO context, Format + lint all files

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,87 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Boys Gone Wild Podcast" },
+    { name: "description", content: "Boys gone wild podcast fan site" },
+    {
+      name: "keywords",
+      content:
+        "Boys gone wild podcast, horatio gould, andrew kirwan, uk comedy podcast",
+    },
+    { name: "robots", content: "noindex, follow" },
+    {
+      tagName: "link",
+      rel: "canonical",
+      href: "https://www.boysgonewildpodcast.com",
+    },
+
+    // Twitter
+    {
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      property: "twitter:site",
+      content: "@horatiothegould",
+    },
+    {
+      property: "twitter:creator",
+      content: "@jameswattnz",
+    },
+    {
+      property: "twitter:title",
+      content: "Boys Gone Wild Podcast",
+    },
+    {
+      property: "twitter:description",
+      content: "Boys gone wild podcast fan site",
+    },
+    {
+      property: "twitter:image",
+      content: "",
+    },
+    {
+      property: "twitter:image:alt",
+      content: "Screen shot of boysgonewildpodcast.com",
+    },
+
+    // Open Graph
+    {
+      property: "og:title",
+      content: "Boys Gone Wild Podcast",
+    },
+    {
+      property: "og:description",
+      content: "Boys gone wild podcast fan site",
+    },
+    {
+      property: "og:site_name",
+      content: "Boys gone wild podcast fan site",
+    },
+    {
+      property: "og:locale",
+      content: "en_US",
+    },
+    {
+      property: "og:image",
+      content: "",
+    },
+    {
+      property: "og:image:width",
+      content: "",
+    },
+    {
+      property: "og:image:height",
+      content: "",
+    },
+    {
+      property: "og:image:alt",
+      content: "Screen shot of boysgonewildpodcast.com",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
   ];
 };
 
