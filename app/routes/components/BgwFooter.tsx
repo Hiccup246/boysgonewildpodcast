f9f3350e0b428f03e30eda244f5e67e80f5c5fa2
James Watt <james.watt529@outlook.com>
2025-05-10 19:44:44 +0100

    Complete basic components and layouts for site

---
+++ app/routes/components/BgwFooter.tsx
@@ -0,0 +1,8 @@
+
+export default function BgwFooter() {
+  return (
+        <footer className="flex flex-col items-center gap-9 bg-bgw-purple p-5 font-Shrikhand">
+          A fan site by a loyal Patron ❤️ 
+        </footer>      
+  );
+}
---
+++ app/routes/components/BgwHeader.tsx
@@ -0,0 +1,8 @@
+
+export default function BgwHeader() {
+  return (
+        <header className="flex flex-col items-center gap-9">
+          Test Header
+        </header>      
+  );
+}
