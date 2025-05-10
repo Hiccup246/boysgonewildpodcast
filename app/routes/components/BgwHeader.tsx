a40def4e8f5cba165cbf93720e9cc6244693ce14
James Watt <james.watt529@outlook.com>
2025-05-10 20:04:56 +0100

    Add eader image

--- app/routes/components/BgwHeader.tsx
+++ app/routes/components/BgwHeader.tsx
@@ -1,5 +1,11 @@
+import BoysGoneWildLogo from "../../images/bgw-header.png";
+
 export default function BgwHeader() {
   return (
-    <header className="flex flex-col items-center gap-9">Test Header</header>
+    <header className="flex flex-col items-center gap-9">
+      <div className="w-9/12">
+        <img src={BoysGoneWildLogo} />
+      </div>
+    </header>
   );
 }
