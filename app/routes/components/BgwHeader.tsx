9e1afdab303c604da1d293d1e843d18237cee8b9
James Watt <james.watt529@outlook.com>
2025-05-11 19:08:44 +0100

    Add new header image and add blurr

--- app/routes/components/BgwHeader.tsx
+++ app/routes/components/BgwHeader.tsx
@@ -2,9 +2,12 @@ import BoysGoneWildLogo from "../../images/bgw-header.png";
 
 export const BgwHeader = () => {
   return (
-    <header className="flex flex-col items-center gap-9">
+    <header className="flex flex-col items-center gap-9 p-5">
       <h1 className="w-7/12">
-        <img src={BoysGoneWildLogo} />
+        <img
+          src={BoysGoneWildLogo}
+          className="shadow-bgw-neon-blue shadow-2xl/70 rounded-2xl"
+        />
       </h1>
     </header>
   );
