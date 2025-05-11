1751d593cf875826fd403a21802a1e8d08fe6498
James Watt <james.watt529@outlook.com>
2025-05-11 15:19:07 +0100

    Change size of header

--- app/routes/components/BgwHeader.tsx
+++ app/routes/components/BgwHeader.tsx
@@ -3,7 +3,7 @@ import BoysGoneWildLogo from "../../images/bgw-header.png";
 export const BgwHeader = () => {
   return (
     <header className="flex flex-col items-center gap-9">
-      <h1 className="w-9/12">
+      <h1 className="w-7/12">
         <img src={BoysGoneWildLogo} />
       </h1>
     </header>
