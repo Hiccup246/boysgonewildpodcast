96e726910614fd77bfff84aeede32801a7c453d2
James Watt <james.watt529@outlook.com>
2025-05-11 20:03:55 +0100

    Add animations to socials links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -165,7 +165,7 @@ export default function Index() {
 
             <PodcastAppModal
               trigger={
-                <div className="flex flex-row bg-bgw-purple border-bgw-neon-blue rounded-2xl border-2 py-2 px-4 gap-2">
+                <div className="flex flex-row bg-bgw-purple border-bgw-neon-blue rounded-2xl border-2 py-2 px-4 gap-2 transition-transform duration-150 hover:-translate-y-1 hover:shadow-md">
                   <div>
                     <LuPodcast title="Podcast App Icon" size="1.5em" />
                   </div>
--- app/routes/components/SocialLink.tsx
+++ app/routes/components/SocialLink.tsx
@@ -11,7 +11,7 @@ export const SocialLink = ({
     <a
       href={link}
       target="_blank"
-      className="flex flex-row bg-bgw-purple border-bgw-neon-blue rounded-2xl border-2 py-2 px-4 gap-2"
+      className="flex flex-row bg-bgw-purple border-bgw-neon-blue rounded-2xl border-2 py-2 px-4 gap-2 transition-transform duration-150 hover:-translate-y-1 hover:shadow-md"
       rel="noreferrer"
     >
       <div>{icon}</div>
